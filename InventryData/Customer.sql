4083ad7b755a1d132c130a78305459aca4221865
scnmalviya <36365172+scnmalviya@users.noreply.github.com>
2018-06-06 13:20:17 +0100

    ready for review

--- InventryData/Customer.sql
+++ InventryData/Customer.sql
@@ -3,5 +3,6 @@
 	[Id] INT NOT NULL PRIMARY KEY, 
     [Name] NCHAR(10) NULL, 
     [LastName] VARCHAR(50) NULL, 
-    [FirstName] VARCHAR(50) NULL
+    [FirstName] VARCHAR(50) NULL, 
+    [Address] VARCHAR(50) NULL
 )
